415ab24c90f5eae7491253c35c91a9027fb5dbff
arjanjohan <arjanvanhoogdalem@gmail.com>
2024-09-02 08:51:32 +0200

    chain details

---
+++ extension/packages/nextjs/utils/chainlink/networks.ts
@@ -0,0 +1,66 @@
+// networks.ts
+interface NetworkConfig {
+  name: string;
+  chainId: number;
+  ccipChainId: bigint;
+}
+
+export const networks: NetworkConfig[] = [
+  {
+    name: "Sepolia",
+    chainId: 11155111,
+    ccipChainId: 16015286601757825753n,
+  },
+  {
+    name: "Base Sepolia",
+    chainId: 84532,
+    ccipChainId: 10344971235874465080n,
+  },
+  {
+    name: "Avalanche Fuji",
+    chainId: 43113,
+    ccipChainId: 14767482510784806043n,
+  },
+];
+
+interface ChainlinkAddresses {
+  [network: string]: {
+    router: string;
+    link: string;
+  };
+}
+
+export const chainlinkAddresses: ChainlinkAddresses = {
+  Ethereum: {
+    router: "0x80226fc0Ee2b096224EeAc085Bb9a8cba1146f7D",
+    link: "0x514910771AF9Ca656af840dff83E8264EcF986CA",
+  },
+  "Avalanche Fuji": {
+    router: "0xF694E193200268f9a4868e4Aa017A0118C9a8177",
+    link: "0x0b9d5D9136855f6FEc3c0993feE6E9CE8a297846",
+  },
+  Sepolia: {
+    router: "0x0BF3dE8c5D3e8A2B34D2BEeB17ABfCeBaf363A59",
+    link: "0x779877A7B0D9E8603169DdbD7836e478b4624789",
+  },
+  "Base Sepolia": {
+    router: "0xD3b06cEbF099CE7DA4AcCf578aaebFDBd6e88a93",
+    link: "0xE4aB69C077896252FAFBD49EFD26B5D171A32410",
+  },
+};
+
+interface tokenAddresses {
+  [network: string]: {
+    assets: Record<string, string>;
+  };
+}
+
+export const tokenAddresses: tokenAddresses = {
+  Ethereum: {
+    assets: {
+      LINK: "0x514910771AF9Ca656af840dff83E8264EcF986CA",
+      ETH: "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
+      USD: "0x0000000000000000000000000000000000000348",
+    },
+  },
+};
