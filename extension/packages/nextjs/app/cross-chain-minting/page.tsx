120182222786899780d2795c6f497a36c8c0d3a2
arjanjohan <arjanvanhoogdalem@gmail.com>
2024-09-02 19:14:12 +0200

    empty pages

---
+++ extension/packages/nextjs/app/cross-chain-minting/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CrossChainMintingDocumentation: React.FC = () => {
+  return <h2>Minting</h2>;
+};
+
+export default CrossChainMintingDocumentation;
---
+++ extension/packages/nextjs/app/functions/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ChainlinkFunctions = () => {
+  return <div>ChainlinkFunctions</div>;
+};
+
+export default ChainlinkFunctions;
